a6a70f077c02c20a979f3a8891bfa1d146c94ce2
vladpython22 <72090059+vladpython22@users.noreply.github.com>
2020-09-30 17:38:14 +0300

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,10 @@
 def sport:
-  print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   sport()
