31394414612efa62cf817db8051b5949e2ba6e85
vladpython22 <72090059+vladpython22@users.noreply.github.com>
2020-10-10 21:26:50 +0300

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,13 @@
+import random
 def sport:
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[0])
+  
+  last = len(quotes) - 1
+  q = random.randint(0, last)
+  print(quotes[q])
 
 if __name__== "__main__":
   sport()
